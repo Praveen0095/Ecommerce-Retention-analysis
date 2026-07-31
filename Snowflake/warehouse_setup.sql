c1b416577e43eda6d13021c4ffdf0fcce4e58989
Praveen0095 <sivapraveen2158hsr@gmail.com>
2026-07-31 21:37:34 +0530

    Update warehouse_setup.sql

--- Snowflake/warehouse_setup.sql
+++ Snowflake/warehouse_setup.sql
@@ -14,5 +14,6 @@ CREATE OR REPLACE SCHEMA retention_analysis.gold_layer;
 -- Create Stage
 -- ======================================================
 CREATE OR REPLACE STAGE retention_analysis.gold_layer.s3_gold_stage
-URL = 's3://looker-gold/'
-CREDENTIALS = (AWS_KEY_ID = 'A*******' AWS_SECRET_KEY = 'T******');
+  URL = 's3://looker-gold/'
+  CREDENTIALS = (AWS_KEY_ID = 'A*******' AWS_SECRET_KEY = 'T******');
+  
